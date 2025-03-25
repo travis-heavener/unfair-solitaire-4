03a78757181d58478108748ab40ab42c74288880
Travis Heavener <travis.heavener@gmail.com>
2025-03-25 03:21:38 -0400

    Add ace & empty deck stacks

--- js/index.mjs
+++ js/index.mjs
@@ -2,8 +2,14 @@ import { CardStack } from "./card-stack.mjs";
 import { generateCards } from "./toolbox.mjs";
 // Store a reference to all card stacks in the game
 const cardStacks = {
-    "aces": [],
-    "stack": new CardStack($("#deck-stack")[0]),
+    "aces": [
+        new CardStack($(".ace-stack")[0]),
+        new CardStack($(".ace-stack")[1]),
+        new CardStack($(".ace-stack")[2]),
+        new CardStack($(".ace-stack")[3])
+    ],
+    "emptyDeck": new CardStack($("#deck-empty-stack")[0]),
+    "deck": new CardStack($("#deck-stack")[0]),
     "board": [
         new CardStack($(".column")[0]),
         new CardStack($(".column")[1]),
@@ -24,7 +30,6 @@ const startGame = () => {
     // Generate cards
     const cards = [];
     generateCards(cards);
-    console.log(cards);
     // Fill columns
     for (let c = 0, i = 0; c < 7; ++c) {
         for (let r = 0; r < c + 1; ++r) {
@@ -37,5 +42,5 @@ const startGame = () => {
     }
     // Push remaining cards to the deck
     for (let i = 51; i >= 28; --i)
-        cardStacks.stack.push(cards[i]);
+        cardStacks.deck.push(cards[i]);
 };
